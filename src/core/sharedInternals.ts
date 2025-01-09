97e007da5a7f968796342d2b8e1453e47b956223
d5ng <dong5ffice@gmail.com>
2025-01-09 22:33:39 +0900

    sharedInternals 주석 추가

--- src/core/sharedInternals.ts
+++ src/core/sharedInternals.ts
@@ -1,6 +1,6 @@
 export type Renderable = { render: () => void }
 
-// Note: Internals 객체는 중요한 내부구현 객체입니다. 절대 변경하지 마세요.
+// Note: Internals 객체는 렌더링에 대한 중요한 내부구현 객체입니다. 직접적으로 절대 변경하지 마세요.
 export interface Internals {
   rootComponent: (() => Renderable) | null
   states: any[]
